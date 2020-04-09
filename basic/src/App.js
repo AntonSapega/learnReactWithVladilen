697bacf2bb120f4132844b2e1e3316a3270bf903
Anton Sapega <antonsapega@gmail.com>
2020-04-09 17:36:39 +0300

    I began learn Chapter 3 about 'state'

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,28 +1,39 @@
 import React from 'react';
 import Car from './Car/Car';
 
-function App() {
-  const CarRepresentation = () => {
+
+class App extends React.Component {
+  state = {
+    cars: [
+      {name: 'Ford', year: 2019},
+      {name: 'Audi', year: 2008},
+      {name: 'Mazda', year: 2003},
+    ],
+    pageTitle: 'The Cars List'
+  }
+
+
+
+  render() {
+
+    const stylesOfCars = {
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    }
+
+    const car = this.state.cars
+
+
     return (
-      <h1>The Cars List</h1>
+      <div style={stylesOfCars}>
+        <h1>{this.state.pageTitle}</h1>
+        <Car name={car[0].name} year={car[0].year} />
+        <Car name={car[1].name} year={car[1].year} />
+        <Car name={car[2].name} year={car[2].year} />
+      </div>
     )
-  }
 
-  const stylesOfCars = {
-    marginLeft: 'auto',
-    marginRight: 'auto',
   }
-
-  return (
-  <div style={ stylesOfCars }>
-    <CarRepresentation />
-    <Car name={'Ford'} year={'2019'}/>
-    <Car name={'Audi'} year={'2008'}>
-      <p>Hello mother fucker</p>
-    </Car>
-    <Car name={'Mazda'} year={'2003'}/>
-  </div>
-  )
 }
 
 export default App;
